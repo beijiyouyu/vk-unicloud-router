ec45d7ca681cebd6e8ba4a1eb601823c6b64f633
VK <370725567@qq.com>
2020-09-30 13:53:00 +0800

    1.3.11

--- pages_template/uni-id/index/index.vue
+++ pages_template/uni-id/index/index.vue
@@ -4,7 +4,7 @@
 			当前集成的
 			<text style="font-weight: bold;margin: 0 10rpx;">uni-id</text>
 			版本：
-			<text style="font-weight: bold;">1.1.9</text> 
+			<text style="font-weight: bold;">1.1.11</text> 
 		</view>
 		
 		<button type="default" @tap="pageTo('../password/password')">用户名和密码</button>
