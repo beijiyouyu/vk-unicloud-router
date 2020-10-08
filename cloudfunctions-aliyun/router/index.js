6c63fa6330496adde9a9d3ddb0747ed1d1170455
VK <370725567@qq.com>
2020-10-08 10:20:01 +0800

    1.4.1

--- cloudfunctions-aliyun/router/index.js
+++ cloudfunctions-aliyun/router/index.js
@@ -1,62 +1,19 @@
 'use strict';
 // 注意：此为云函数路由入口文件，请勿修改此文件代码，你自己的云函数逻辑应写在service目录下
-const vk = require('vk-unicloud');    // vk-unicloud 工具包
+const vk = require('vk-unicloud');                     // vk-unicloud 工具包
+const pubFun = require(__dirname+'/util/pubFunction');   // __dirname是为了兼容阿里云
 const config = require('config');     // 全局配置信息
-const pubFun = require(__dirname+'/util/pubFunction'); // __dirname是为了兼容阿里云
-const uniID = require('uni-id');  // uni-id 公共模块
+const uniID = require('uni-id');      // uni-id 公共模块
 uniID.init(config["uni"]);
 const db = uniCloud.database();
-process.env.TZ ='Asia/Shanghai';  // 设置时区为东八区(目前无效)
+
+vk.init({
+	config,
+	uniID,
+	db,
+	pubFun,
+	//customUtil : customUtil
+});
 exports.main = async (event, context) => {
-	let originalParam = {event, context};
-	let param = vk.getQueryStringParameters(event);
-	let { url, data, uniIdToken } = param;
-	// 工具包
-	let util = {
-		vk, config, pubFun, uniID, db,
-		_:db.command
-	};
-	let serviceParam = {
-		url, data, uniIdToken, util, originalParam
-	};
-	// 全局过滤器
-	const filterService = vk.require('util/filter');
-	let res = await filterService.main(serviceParam);
-	if(res.code != 0) return res;		// code不为0，则直接返回错误信息
-	if(res.uid) data.uid = res.uid;	// 如果存在uid，则参数自动加上uid参数
-	// 加载业务函数
-	let serviceMain;
-	try{
-		serviceMain = vk.require('service/'+url);
-	}catch(err){
-		return {
-			code:404, msg:'云函数'+url+'不存在', err:err
-		};
-	}
-	// 运行业务函数，并返回结果给前端
-	return await serviceRun({
-		res,
-		serviceParam,
-		serviceMain
-	});
+	return await vk.router({ event, context, vk });
 };
-
-
-// 运行业务函数
-async function serviceRun(obj = {}){
-	let { 
-		res, 
-		serviceParam, 
-		serviceMain 
-	} = obj;
-	if(res.uid) serviceParam.uid = res.uid;
-	if(res.userInfo) serviceParam.userInfo = res.userInfo;
-	let returnRes = await serviceMain.main(serviceParam);
-	if(res.token && typeof returnRes == "object"){
-		returnRes.vk_uni_token = {
-			token : res.token,
-			tokenExpired : res.tokenExpired
-		};
-	}
-	return returnRes;
-}
--- main.js
+++ main.js
@@ -9,39 +9,31 @@ Vue.use(vk);
 
 // 自定义云函数路由配置
 Vue.prototype.vk.callFunctionUtil.setConfig({
-		functionNameToUrl:{
-			// 云函数路由（主函数url化地址）
-			"router":"https://bbdf976b-a6a6-42b2-9429-db232ee80f13.bspapp.com/http/router",
-			// 云函数路由（开发测试函数url化地址）
-			"router-test":"https://bbdf976b-a6a6-42b2-9429-db232ee80f13.bspapp.com/http/router-test"
-		}
+	debug:true,
+	loginPagePath:"/pages_template/uni-id/login/index/index",
+	functionNameToUrl:{
+		// 云函数路由（主函数url化地址）
+		"router":"https://bbdf976b-a6a6-42b2-9429-db232ee80f13.bspapp.com/http/router",
+		// 云函数路由（开发测试函数url化地址）
+		"router-test":"https://bbdf976b-a6a6-42b2-9429-db232ee80f13.bspapp.com/http/router-test"
+	}
 });
 
 // 自定义token拦截器
-Vue.prototype.vk.callFunctionUtil.interceptor.login = (obj = {}) =>{
-  let { params, res } = obj;
-  setTimeout(function(){
-    uni.navigateTo({
-      url:"/pages/login/index/index",
-      events:{
-        // 监听登录成功后的事件
-        loginSuccess: function(data) {
-        // 重新执行一次云函数调用
-          if(params) Vue.prototype.vk.callFunction(params);
-        }
-      }
-    });
-  },300); 
-  console.log("跳自己的登录页面");
-  // 上方代码可自己修改，写成你自己的逻辑处理。
-};
-
+// Vue.prototype.vk.callFunctionUtil.interceptor.login = (obj = {}) =>{
+//   let { params, res } = obj;
+// 	if(!params.noAlert){
+// 		Vue.prototype.vk.alert(res.result.msg);
+// 	}
+//   console.log("跳自己的登录页面");
+//   // 上方代码可自己修改，写成你自己的逻辑处理。
+// };
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
