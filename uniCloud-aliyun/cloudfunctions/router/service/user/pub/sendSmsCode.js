1e1c8429dbb4b88b512c6b8f3b35f85cfa9976a2
VK云桌面 <370725567@qq.com>
2022-08-19 11:14:54 +0800

    2.10.11

--- uniCloud-aliyun/cloudfunctions/router/service/user/pub/sendSmsCode.js
+++ uniCloud-aliyun/cloudfunctions/router/service/user/pub/sendSmsCode.js
@@ -6,6 +6,8 @@ module.exports = {
 	 * data 请求参数 说明
 	 * @param {String} mobile 手机号
 	 * @param {String} type  验证码类型
+	 * @param {Boolean} checkUserExist 是否需要检测手机号对应的账号是否存在，默认false：不检测 设置为true：会检测，如果检测到用户不存在，则不发短信。
+	 * 
 	 * res 返回参数说明
 	 * @param {Number} code 错误码，0表示成功
 	 * @param {String} msg 详细信息
@@ -18,11 +20,25 @@ module.exports = {
 		let { uid } = data;
 		let res = { code: 0, msg: '' };
 		// 业务逻辑开始-----------------------------------------------------------
-		let { mobile, type } = data;
+		let { mobile, type, checkUserExist = false } = data;
 		if (vk.pubfn.isNull(mobile)) {
 			return { code: -1, msg: '手机号不能为空' };
 		}
-		const code = vk.pubfn.random(6, "0123456789");
+		if (!vk.pubfn.test(mobile, 'mobile')) {
+			return { code: -1, msg: "手机号格式错误" };
+		}
+		if (checkUserExist) {
+			let dcloud_appid = originalParam.context.appId;
+			// 检测手机号对应的账号是否存在
+			let num = await vk.daoCenter.userDao.count({
+				mobile,
+				dcloud_appid: _.or(_.eq(dcloud_appid), _.exists(false))
+			});
+			if (num === 0) {
+				return { code: -1, msg: `手机号：${mobile} 未注册` };
+			}
+		}
+		let code = vk.pubfn.random(6, "0123456789");
 		let provider = "unicloud";
 		if (vk.pubfn.getData(config, "vk.service.sms.aliyun.enable")) {
 			provider = "aliyun";
