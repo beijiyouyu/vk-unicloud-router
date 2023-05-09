70fbff7ff6bd6a1af492262b1d3aea8526f14c90
VK云桌面 <370725567@qq.com>
2023-05-09 11:46:49 +0800

    更新依赖

--- uni_modules/uni-config-center/uniCloud/cloudfunctions/common/uni-config-center/vk-unicloud/index.js
+++ uni_modules/uni-config-center/uniCloud/cloudfunctions/common/uni-config-center/vk-unicloud/index.js
@@ -83,7 +83,7 @@ module.exports = {
 		},
 		"db": {
 			"unicloud": {
-				"maxLimit": 500, // 最大limit限制(目前腾讯云最大1000,阿里云最大500)
+				"maxLimit": 1000, // 最大limit限制(目前腾讯云最大1000,阿里云最大1000)
 				"cancelAddTime": false, // 取消 vk.baseDao.add 时自动生成 _add_time 和 _add_time_str
 				"cancelAddTimeStr": false, // 仅取消 vk.baseDao.add 时自动生成的 _add_time_str，若已设置 cancelAddTime 为true，则 cancelAddTimeStr 也会强制为true
 				"getTableData": {
