d2f1553ea34258d4afd36307627ed64251e9e9f4
VK云桌面 <370725567@qq.com>
2023-09-23 17:03:21 +0800

    【优化】如果当前运行的微信小程序是体验版，但没有点HBX的【发行】菜单发布，则提示开发者请点【发行】按钮

--- App.vue
+++ App.vue
@@ -29,7 +29,18 @@
 			// #endif
 		},
 		onShow: function() {
-			if(config.debug) console.log('App Show');
+			if (config.debug) console.log("App Show");
+			// #ifdef MP-WEIXIN
+			// 检测如果当前是体验版，但没有点HBX的【发行】菜单发布，则提示开发者请点【发行】按钮
+			try {
+				console.log('当前运行模式: ', process.env.NODE_ENV);
+				const info = uni.getAccountInfoSync();
+				if (info.miniProgram.envVersion === "trial" && process.env.NODE_ENV !== 'production') {
+					// 如果不想要alert弹窗提示，可以将vk.alert改成console.log
+					uni.vk.alert('检测到您当前发布的【体验版】是通过【运行】按钮打包发布的，请重新点击HBX上方菜单的【发行】按钮进行打包发布小程序（如果确定是【发行】按钮打包的，请删除手机上的体验版小程序并重新扫码进入）', '重要提示', '好的');
+				}
+			} catch(err){}
+			// #endif
 		},
 		onHide: function() {
 			if(config.debug) console.log('App Hide');
